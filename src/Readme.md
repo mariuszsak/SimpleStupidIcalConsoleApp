e59d7a41e2c6cb5691a05a2baae996a7b2dd210d
thornhell <mariuszsak@gmail.com>
2019-04-03 23:13:33 +0200

    first commit

---
+++ src/Readme.md
@@ -0,0 +1,5 @@
+## Simple Stupid Icalendar Console Application
+
+There is just a simple stupid Icalendar console application. 
+
+I have assumed that all values passing to the application would be valid.
---
+++ src/model/IcalendarStorage.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.util.UUID;
+
+public class IcalendarStorage {
+
+    private String eventName;
+    private String eventStart;
+    private String eventEnd;
+    private UUID uuid;
+    private String tstamp;
+    private String eventLocation;
+    private String eventDescription;
+
+
+    public IcalendarStorage() {
+    }
+
+
+    public IcalendarStorage(String eventName, String eventStart, String eventEnd, UUID uuid, String tstamp, String eventLocation, String eventDescription) {
+        this.eventName = eventName;
+        this.eventStart = eventStart;
+        this.eventEnd = eventEnd;
+        this.uuid = uuid;
+        this.tstamp = tstamp;
+        this.eventLocation = eventLocation;
+        this.eventDescription = eventDescription;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventStart() {
+        return eventStart;
+    }
+
+    public void setEventStart(String eventStart) {
+        this.eventStart = eventStart;
+    }
+
+    public String getEventEnd() {
+        return eventEnd;
+    }
+
+    public void setEventEnd(String eventEnd) {
+        this.eventEnd = eventEnd;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTstamp() {
+        return tstamp;
+    }
+
+    public void setTstamp(String tstamp) {
+        this.tstamp = tstamp;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+}
